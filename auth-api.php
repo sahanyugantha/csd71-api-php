c030260ccd534e4da9cafd8a31914380446f9f3e
Sahan Perera <sahanyuganthamail@gmail.com>
2022-07-03 17:09:23 +0530

    auth-api responses updated

--- auth-api.php
+++ auth-api.php
@@ -6,8 +6,8 @@
 		$dataStr = file_get_contents("php://input");
 		$authData = json_decode($dataStr);
 
-		$u_email = $authData->email;
-		$u_password = sha1($authData->password);
+		$u_email = urldecode($authData->email);
+		$u_password = sha1(urldecode($authData->password));
 
 		$sql = "SELECT * FROM `tbl_user` WHERE `email`=? AND `password`=?";
 
@@ -21,17 +21,16 @@
 			if($rows > 0){
 
 				$dataObj = array();
+				$userObj = array();
 				while(mysqli_stmt_fetch($stmt)){
-					$userObj = array();
 					$userObj["id"] = $id;
 					$userObj["email"] = $email;
 					//$userObj["password"] = $password;
 					$userObj["gender"] = $gender;
 					$userObj["mobile"] = $mobile;
-
-					array_push($dataObj, array("USER"=>$userObj));
 				}
-				array_push($dataObj, array("SUCCESS"=>"Successfully logged in!"));
+				array_push($dataObj, array("AUTH"=>"SUCCESS"));
+				array_push($dataObj, array("USER"=>$userObj));
 
 				ob_start();
 				header("Content-Type:application/json");
@@ -39,10 +38,12 @@
 				ob_end_flush();
 				
 			} else {
-				$msg = array("FAILED" => "Authentication failed!");
+				$dataObj = array();
+			//	$msg = array("AUTH" => "FAILED");
+				array_push($dataObj,  array("AUTH" => "FAILED"));
 				ob_start();
 				header("Content-Type:application/json");
-				echo json_encode($msg);
+				echo json_encode($dataObj);
 				ob_end_flush();
 			}
 
